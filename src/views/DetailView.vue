0b45a0e66e94d64784b781d2d26cb41c03ab2c9d
wonyoung <elese0821@naver.com>
2023-11-29 17:34:42 +0900

    dd

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -46,22 +46,22 @@ export default {
                 </nav>
             </div>
             <div class="header__intro"
-                style="backgroundImage:url(https://image.tmdb.org/t/p/w500/kjQBrc00fB2RjHZB3PGR4w9ibpz.jpg)">
+                :style="movieInfo.backdrop_path ? {
+                    backgroundImage: 'url(https://image.tmdb.org/t/p/w500' + movieInfo.backdrop_path) '} : {}">
                 <div class="container">
                     <div class="left play__icon">
                         <a href="#">
-                            <img src="https://image.tmdb.org/t/p/w500/vFsSluuzqxR46Ils9ib52ItdE9u.jpg" alt="dd">
+                            <img :src="'https://image.tmdb.org/t/p/w500' + movieInfo.poster_path"
+                                :alt="movieInfo.original_title">
                         </a>
                     </div>
                     <div class="right">
-                        <h2>{{ movieInfo.original_title }}</h2>
+                        <h2>{{ movieInfo.title }}</h2>
                         <p class="desc">
-                            인류를 지키기 위해 만들어진 AI가 LA에 핵폭탄을 터뜨린 후, 인류와 AI 간의 피할 수 없는 전쟁이 시작된다. 전직 특수부대 요원 ‘조슈아’는
-                            실종된 아내의 단서를 얻을지도 모른다는 생각에 전쟁을 끝내기 위한 인류의 작전에 합류한다. 인류를 위협할 강력한 무기와 이를 창조한 ‘창조자’를 찾아 나서고, 그 무기가
-                            아이 모습의 AI 로봇 '알피'란 사실을 알게 되는데… 인간적인가, 인간의 적인가? 10월, SF 블록버스터의 신세계가 펼쳐진다!
+                            {{ movieInfo.overview }}
                         </p>
-                        <p class="date">개봉 : 2023-09-27</p>
-                        <p class="average">평점 : 7.1</p>
+                        <p class="date">개봉 : {{ movieInfo.release_date }}</p>
+                        <p class="average">평점 : {{ movieInfo.vote_average }}</p>
                     </div>
                 </div>
             </div>
